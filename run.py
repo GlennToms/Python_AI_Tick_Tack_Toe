9a2f60676ad2a8439a5f56d811e0fca74300a203
Glenn Toms <Glenn@glenntoms.gg>
2021-04-07 20:15:41 +0000

    Converting to Tensor

--- run.py
+++ run.py
@@ -1,16 +1,65 @@
 import random
 import numpy as np
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import median, mean
+from collections import Counter
+
+
+LR = 1e-3
+goal_steps = 500
+score_requirement = 5
+initial_games = 10000
 
 
 class Game:
-    def __init__(self, rows: int, cols: int, auto_reset: bool = False, show_only_end: bool = False):
+    def __init__(self, rows: int, cols: int, auto_reset: bool = False, show_only_end: bool = False, render=False):
         self.rows = rows
         self.cols = cols
         self.state = np.full(shape=(rows, cols), fill_value=None)
         self.player = True
+        self.winner = None
         self.auto_reset = auto_reset
         self.game_over = False
         self.show_only_end = show_only_end
+        self.render = render
+
+    def reset(self):
+        self.state = np.full(shape=(self.rows, self.cols), fill_value=None)
+        self.game_over = False
+        self.player = True
+
+    def flat_state(self):
+        return [y for x in self.state for y in x]
+
+    def step(self, pos: int):
+        pos = pos + 1
+        row = 0
+        col = pos
+
+        # TODO: This is hard codes for 3x3
+        if pos >= self.rows:
+            row = 1
+            col = pos - self.rows
+            if pos >= (self.rows * 2):
+                row = 2
+                col = pos - (self.rows * 2)
+        placed_okay = self.place(col, row)
+        reward = 1 if placed_okay else -1
+        if placed_okay:
+            self.place(random.randrange(0, self.rows), random.randrange(0, self.cols))
+        observation = self.flat_state()
+        done = self.game_over
+        if done and self.winner:
+            reward + 1
+        elif done and self.winner is None:
+            reward = 0
+        elif done and self.winner is False:
+            reward = -1
+        info = None
+
+        return (observation, reward, done, info)
 
     def place(self, col: int, row: int) -> bool:
         """Returns True placement was acceptable"""
@@ -20,16 +69,21 @@ class Game:
             char = "x"
 
         if self.state[col, row] is None:
+
             self.state[col, row] = char
             if self.show_only_end is False:
-                self.print()
+                if self.render:
+                    self.print()
 
             won = self.check_if_game_over()
 
             if won == -1:
                 if self.show_only_end:
-                    self.print()
-                print(f"Game is a Draw!")
+                    if self.render:
+                        self.print()
+                if self.render:
+                    print(f"Game is a Draw!")
+                self.winner = None
                 self.game_over = True
                 if self.auto_reset:
                     self.state = np.full(shape=(self.rows, self.cols), fill_value=None)
@@ -38,11 +92,14 @@ class Game:
 
             elif won is True:
                 if self.show_only_end:
-                    self.print()
+                    if self.render:
+                        self.print()
                 char = "o"
                 if self.player:
+                    self.winner = self.player
                     char = "x"
-                print(f"'{char}' has won!")
+                if self.render:
+                    print(f"'{char}' has won!")
                 self.game_over = True
 
                 if self.auto_reset:
@@ -57,14 +114,15 @@ class Game:
         return False
 
     def print(self):
-        for x in self.state:
-            for y in x:
-                if y is None:
-                    print(f"|   ", end="")
-                else:
-                    print(f"| {y} ", end="")
-            print("|")
-        print("-------------")
+        if self.render:
+            for x in self.state:
+                for y in x:
+                    if y is None:
+                        print(f"|   ", end="")
+                    else:
+                        print(f"| {y} ", end="")
+                print("|")
+            print("-------------")
 
     def check_if_game_over(self) -> bool:
         """returns True if game is over"""
@@ -115,12 +173,97 @@ class Game:
         return not self.game_over
 
 
-if __name__ == "__main__":
-    game = Game(rows=3, cols=3, auto_reset=True, show_only_end=True)
-    while game:
-        while game.place(random.randrange(0, game.rows), random.randrange(0, game.cols)) is not False:
-            import time
+game: Game = None
 
-            time.sleep(0.1)
-            pass
-    print("Gamed has Ended")
+
+def initial_population():
+    # [OBS, MOVES]
+    training_data = []
+    # all scores:
+    scores = []
+    # just the scores that met our threshold:
+    accepted_scores = []
+    # iterate through however many games we want:
+    for _ in range(initial_games):
+        score = 0
+        # moves specifically from this environment:
+        game_memory = []
+        # previous observation that we saw
+        prev_observation = []
+        # for each frame in 200
+        for _ in range(goal_steps):
+            # choose random action (0 or 1)
+            action = random.randrange(0, (game.rows * game.cols) - 1)
+            # do it!
+            observation, reward, done, info = game.step(action)
+
+            # notice that the observation is returned FROM the action
+            # so we'll store the previous observation here, pairing
+            # the prev observation to the action we'll take.
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            score += reward
+            if done:
+                break
+
+        # IF our score is higher than our threshold, we'd like to save
+        # every move we made
+        # NOTE the reinforcement methodology here.
+        # all we're doing is reinforcing the score, we're not trying
+        # to influence the machine in any way as to HOW that score is
+        # reached.
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                # convert to one-hot (this is the output layer for our neural network)
+                if data[1] == 0:
+                    output = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+                elif data[1] == 1:
+                    output = [0, 1, 0, 0, 0, 0, 0, 0, 0]
+                elif data[1] == 2:
+                    output = [0, 0, 1, 0, 0, 0, 0, 0, 0]
+                elif data[1] == 3:
+                    output = [0, 0, 0, 1, 0, 0, 0, 0, 0]
+                elif data[1] == 4:
+                    output = [0, 0, 0, 0, 1, 0, 0, 0, 0]
+                elif data[1] == 5:
+                    output = [0, 0, 0, 0, 0, 1, 0, 0, 0]
+                elif data[1] == 6:
+                    output = [0, 0, 0, 0, 0, 0, 1, 0, 0]
+                elif data[1] == 7:
+                    output = [0, 0, 0, 0, 0, 0, 0, 1, 0]
+                elif data[1] == 8:
+                    output = [0, 0, 0, 0, 0, 0, 0, 0, 1]
+
+                # saving our training data
+                training_data.append([data[0], output])
+
+        # reset env to play again
+        game.reset()
+        # save overall scores
+        scores.append(score)
+
+    # just in case you wanted to reference later
+    training_data_save = np.array(training_data)
+    np.save("saved.npy", training_data_save)
+
+    # some stats here, to further illustrate the neural network magic!
+    print("Average accepted score:", mean(accepted_scores))
+    print("Median score for accepted scores:", median(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
+
+
+if __name__ == "__main__":
+    # game = Game(rows=3, cols=3, auto_reset=True, show_only_end=False)
+    # while game:
+    #     observation, reward, done, info = game.step(random.randrange(0, game.rows), random.randrange(0, game.cols))
+    # print("Gamed has Ended")
+    pass
+
+
+game = Game(3, 3, show_only_end=True)
+training_data = initial_population()
+print("DONE")
